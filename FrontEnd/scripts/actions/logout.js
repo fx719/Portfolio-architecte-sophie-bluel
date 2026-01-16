699107e8e495b6be91f374765b22e2a231a6094c
fx719 <frederic.dupont78@yahoo.fr>
2026-01-16 11:17:14 +0100

    doc(app.js) : comment this file to make it easier to read.

--- FrontEnd/scripts/actions/logout.js
+++ FrontEnd/scripts/actions/logout.js
@@ -1,3 +1,11 @@
+/**
+ * This function has to be used inside a callback function of an event listener.
+ * Dynamically sets the homepage of a logged user back to the non-logged user interface, and deletes the session storage data.
+ * @param {Event} e 
+ * @param {HTMLElement} editModBanner 
+ * @param {HTMLElement} editFormLinkContent 
+ * @param {HTMLElement} buttonDisplayAllProjects 
+ */
 function logout(e, editModBanner, editFormLinkContent, buttonDisplayAllProjects) {
     e.preventDefault()
     sessionStorage.clear()
--- FrontEnd/scripts/app.js
+++ FrontEnd/scripts/app.js
@@ -4,6 +4,7 @@ const authLink = document.getElementById('auth-link')
 const header = document.querySelector("header")
 const projectsTitle = document.querySelector(".projects-title")
 
+//Contacts the API to get the works'data, then displays them.
 let works = []
 getDataFromAPI("http://127.0.0.1:5678/api/works")
     .then(data => {
@@ -12,7 +13,7 @@ getDataFromAPI("http://127.0.0.1:5678/api/works")
     })
 
 
-
+//Contacts the API to get the categories'data, used to display the filter's button, then displays them.
 let categories = []
 getDataFromAPI("http://127.0.0.1:5678/api/categories")
     .then(data => {
@@ -27,23 +28,36 @@ getDataFromAPI("http://127.0.0.1:5678/api/categories")
         }
     })
 
+
 if (isAuthentified) {
+
     authLink.setAttribute("href", "#")
     authLink.innerText = "logout"
     addEditModBanner(header)
+
     const buttonDisplayAllProjects = document.getElementById('0')
     buttonDisplayAllProjects.removeAttribute("autofocus")
     categoriesButtonsDiv.setAttribute("style", "display:none;")
     addEditFormLink(projectsTitle)
+
+    //Identifies this element to hide it when the user logs out.
     const editModBanner = document.querySelector(".edit-mod-banner")
+
+    //Identifies this element to display the first modal-window
     const editFormLinkContent = document.querySelector(".edit-form-link-content")
-    authLink.addEventListener("click", (e) => {
-        logout(e, editModBanner, editFormLinkContent, buttonDisplayAllProjects)
-    }, { once: true })
+
+
+    //Displays modal-window 
     let modal = null
     const editFormLink = document.getElementById('edit-form-link')
     editFormLink.addEventListener("click", displayModal)
     const grid = document.querySelector(".projects-photo-grid")
+
+    //Event listener to logout the logged-in user.
+    authLink.addEventListener("click", (e) => {
+        logout(e, editModBanner, editFormLinkContent, buttonDisplayAllProjects)
+    }, { once: true })
+
 } else {
 
 }
